e89045d57600318518fa2c31b089a2f321ee99f3
linHeMa <linhema0921@gmail.com>
2022-08-01 18:15:44 +0800

    ajax table

---
+++ week-2/table.js
@@ -0,0 +1,27 @@
+const dataUrl =
+  "https://appworks-school.github.io/Remote-Aassigiment-Data/products";
+const xhr = new XMLHttpRequest();
+
+function ajax(url) {
+  return fetch(url);
+}
+ajax(dataUrl)
+  .then((response) => {
+    return response.json();
+  })
+  .then((response) => render(response))
+  .catch((error) => {
+    console.log(`Error: ${error}`);
+  });
+function render(data) {
+  let words = "";
+  for (ele of data) {
+    words += `<tr>
+    <td>${ele.name}</td>
+    <td>${ele.description}</td>
+    <td>${ele.price}</td>
+    </tr>
+    `;
+  }
+  document.querySelector(".table tbody").innerHTML = words;
+}
