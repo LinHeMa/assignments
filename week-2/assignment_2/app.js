27dda3b3809efb69cd429a7e803906e4f076a1ef
linHeMa <linhema0921@gmail.com>
2022-08-05 11:00:41 +0800

    part 5 first commit

--- week-2/assignment_2/app.js
+++ week-2/assignment_2/app.js
@@ -26,8 +26,46 @@ app.use((req, res, next) => {
   next()
 })
 
+<<<<<<< Updated upstream
 app.use((err, req, res, next) => {
   res.render('error', err)
+=======
+app.get('/', (req, res) => {
+  const name = req.cookies.username
+  name ? res.render('index', { name }) : res.redirect('/hello')
+})
+
+app.get('/getData', (req, res) => {
+  const isNum = !isNaN(req.query.number)
+  const querynumber = Number(req.query.number)
+  const total = ((querynumber + 1) * querynumber) / 2
+  if (req.query.number === undefined || req.query.number === '') res.send({ message: 'Lack of Parameter' })
+  isNum ? res.send({ message: total }) : res.send({ message: 'Wrong parameter' })
+})
+
+app.get('/cards', (req, res) => {
+  res.locals.prompt = '誰是馬伊隆？'
+  res.locals.hint = '騷兩圈'
+  res.locals.colors = colors
+  res.render('flashcard')
+})
+
+app.get('/hello', (req, res) => {
+  const name = req.cookies.username
+  name ? res.redirect('/') : res.render('hello')
+})
+
+app.post('/hello', (req, res) => {
+  const name = req.body.username
+  res.cookie('username', name)
+  res.redirect('/')
+})
+
+app.post('/goodbye', (req, res) => {
+  res.clearCookie('username')
+  res.redirect('/')
+  console.dir('cookies clear!')
+>>>>>>> Stashed changes
 })
 
 app.listen(mainPort, () => console.log('這個app在localhost:3000上運行！！'))
